9004b6ca188fc45a07117f9469bd1a8734564563
Fathi Abdul Rahim <fathiabdrahim@yahoo.com>
2016-11-10 17:05:45 +0800

    update api

--- lib/billplz/collection.rb
+++ lib/billplz/collection.rb
@@ -3,15 +3,15 @@ module Billplz
     before_request :set_base_url
     #before_request :replace_body
 
-    post :create, ""
+    post :create, "/collections/"
     post :create_open_collection, "/open_collections"
-    post :deactivate, "collections/:id/deactivate"
-    post :activate, "collections/:id/activate"
+    post :deactivate, "/collections/:id/deactivate"
+    post :activate, "/collections/:id/activate"
 
     private
 
     def set_base_url(name, request)
-      Flexirest::Base.base_url = "#{Billplz.configuration.api_url}/"
+      Flexirest::Base.base_url = "#{Billplz.configuration.api_url}"
     end
 
   end
