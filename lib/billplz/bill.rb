035f468afa6e22275f5df80a82573b0d783e144c
Khairi Adnan <khairi@labs.my>
2017-03-22 12:46:20 +0800

    Update bill.rb

--- lib/billplz/bill.rb
+++ lib/billplz/bill.rb
@@ -1,23 +1,10 @@
 module Billplz
   class Bill < Model
 
-    before_request :set_base_url
-    before_request :replace_body
-
     get :find, "/bills/:id"
     get :transactions, "/bills/:bill_id/transactions"
     post :create, "/bills"
     delete :delete, "/bills/:id"
-
-    private
-
-    def replace_body(name, request)
-      if name == :create
-        #add collection id here
-        request.post_params = request.post_params.merge(collection_id: "#{ENV['BILLPLZ_COLLECTION_ID']}")
-        request.body = request.post_params.to_json
-      end
-    end
 
   end
 end
